525df9f63184e1a74eb09879c263dd48816f6adc
S F <27873085+cybershuimo@users.noreply.github.com>
2018-01-11 16:42:24 +0800

    finished 18_key_states, cannot use multiple key press yet

---
+++ ProjectTest/18_key_states/18_test_key_states.cpp
@@ -0,0 +1,429 @@
+// 17_mouse_events, handles mouse events such as mouse motion, mouse button presses, and mouse button releasing.
+// If one of these three mouse events do occur, we check the mouse position using SDL_GetMouseState. 
+// Depending on whether the mouse is over the button or not, we'll want to display different sprites.
+
+//Using SDL, SDL_image, standard IO, and strings
+#include <SDL.h>
+#include <SDL_image.h>
+#include <stdio.h>
+#include <string>
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+//Texture wrapper class
+class LTexture
+{
+    public:
+        //Initializes variables
+        LTexture();
+
+        //Deallocates memory
+        ~LTexture();
+
+        //Loads image at specified path
+        bool loadFromFile( std::string path );
+
+        #ifdef _SDL_TTF_H
+        //Creates image from font string
+        //If SDL_ttf is not defined, ignore this piece of code.
+        bool loadFromRenderedText( std::string textureText, SDL_Color textColor );
+        #endif
+
+        //Deallocates texture
+        void free();
+
+        //Set color modification of texture
+        void setColor( Uint8 red, Uint8 green, Uint8 blue );
+
+        //Set blending
+        void setBlendMode( SDL_BlendMode blending );
+
+        //Set alpha modulation
+        void setAlpha( Uint8 alpha );
+
+        //Renders texture at given point, and whether it is a clip
+        void render( int x, int y, SDL_Rect* clip = NULL, 
+        	double angle = 0.0, SDL_Point* center = NULL, SDL_RendererFlip flip = SDL_FLIP_NONE );
+
+        //Gets image dimensions
+        int getWidth();
+        int getHeight();
+
+    private:
+        //The actual hardware texture
+        SDL_Texture* mTexture;
+
+        //Image dimensions
+        int mWidth;
+        int mHeight;
+};
+
+
+//The window we'll be rendering to
+SDL_Window* gWindow = NULL;
+
+//The window renderer
+SDL_Renderer* gRenderer = NULL;
+
+//Texture to render
+LTexture gUpTexture;
+LTexture gDownTexture;
+LTexture gLeftTexture;
+LTexture gRightTexture;
+LTexture gPressTexture;
+
+// Now to build the class methods
+// Initializes
+LTexture::LTexture()
+{
+    mTexture = NULL;
+    mWidth = 0;
+    mHeight = 0;
+}
+
+// Deallocates texture
+LTexture::~LTexture()
+{
+    //Free loaded image
+    free();
+}
+
+// Load image from specific path
+bool LTexture::loadFromFile( std::string path )
+{
+    //Initialization flag
+    bool success = true;
+    //Get rid of preexisting texture
+    free();
+
+    //Load image at specified path
+    SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
+    if( loadedSurface == NULL )
+    {
+        printf( "Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError() );
+        success = false;
+    }
+    else
+    {
+        //Color key image, use SDL_SetColorKey() to set the color key (transparent pixel) in a surface
+        //SDL_SetColorKey( loadedSurface, SDL_TRUE, SDL_MapRGB( loadedSurface->format, 0, 0, 0 ) );
+        //Create texture from surface pixels
+        mTexture = SDL_CreateTextureFromSurface( gRenderer, loadedSurface );
+        if( mTexture == NULL )
+        {
+            printf( "Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
+            success = false;
+        }
+        else
+        {
+            //Fill width, height of the image
+            mWidth = loadedSurface->w;
+            mHeight = loadedSurface->h;
+
+        }
+
+        //Get rid of old loaded surface
+        SDL_FreeSurface( loadedSurface );
+    }
+
+    return success;
+}
+
+
+#ifdef _SDL_TTF_H
+bool LTexture::loadFromRenderedText( std::string textureText, SDL_Color textColor )
+{
+    //Get rid of preexisting texture
+    free();
+
+    //Render text surface
+    SDL_Surface* textSurface = TTF_RenderText_Solid( gFont, textureText.c_str(), textColor );
+    if( textSurface == NULL )
+    {
+        printf( "Unable to render text surface! SDL_ttf Error: %s\n", TTF_GetError() );
+    }
+    else
+    {
+        //Create texture from surface pixels
+        mTexture = SDL_CreateTextureFromSurface( gRenderer, textSurface );
+        if( mTexture == NULL )
+        {
+            printf( "Unable to create texture from rendered text! SDL Error: %s\n", SDL_GetError() );
+        }
+        else
+        {
+            //Get image dimensions
+            mWidth = textSurface->w;
+            mHeight = textSurface->h;
+        }
+
+        //Get rid of old surface
+        SDL_FreeSurface( textSurface );
+    }
+    
+    //Return success
+    return mTexture != NULL;
+}
+#endif
+
+
+void LTexture::free()
+{
+    //Free texture if it exists
+    if ( mTexture != NULL )
+    {
+    	SDL_DestroyTexture( mTexture );
+	    mTexture = NULL;
+	    mWidth = 0;
+	    mHeight = 0;
+    }
+}
+
+//Renders texture at given point
+void LTexture::render( int x, int y, SDL_Rect* clip, double angle, SDL_Point* center, SDL_RendererFlip flip )
+{
+    //Set destination rectangle
+    SDL_Rect renderQuad = { x, y, mWidth, mHeight };
+
+    //Set renderQuad width and height same as source rect
+    if ( clip != NULL )
+    {
+        renderQuad.w = clip->w;
+        renderQuad.h = clip->h;
+    }
+    
+    SDL_RenderCopyEx( gRenderer, mTexture, clip, &renderQuad, angle, center, flip );
+    //SDL_RenderCopy( renderer, texture, &srcrect, &dstrect );
+}
+
+
+//Set color modulation
+void LTexture::setColor( Uint8 red, Uint8 green, Uint8 blue )
+{
+	SDL_SetTextureColorMod( mTexture, red, green, blue );
+	//srcC = srcC * (color / 255)
+	//return -1 if color modulation is not supported.
+}
+
+//Set alpha modulation
+void LTexture::setAlpha( Uint8 alpha )
+{
+	SDL_SetTextureAlphaMod( mTexture, alpha );
+	//srcA = srcA * (alpha / 255)
+	//return -1 if alpha modulation is not supported.
+}
+
+//Set blending, e.g.alpha blending
+void LTexture::setBlendMode( SDL_BlendMode blending )
+{
+	SDL_SetTextureBlendMode( mTexture, blending );
+}
+
+
+//Get image dimensions
+int LTexture::getWidth()
+{
+    return mWidth;
+}
+
+int LTexture::getHeight()
+{
+    return mHeight;
+}
+
+
+
+bool init()
+{
+    //Initialization flag
+    bool success = true;
+
+    //Initialize SDL
+    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+    {
+        printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
+        success = false;
+    }
+    else
+    {
+        //Create window
+        gWindow = SDL_CreateWindow( "SDL Test_18", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+        if( gWindow == NULL )
+        {
+            printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
+            success = false;
+        }
+        else
+        {
+            //Create vsynced renderer for window
+            gRenderer = SDL_CreateRenderer( gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC );
+            if( gRenderer == NULL )
+            {
+                printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
+                success = false;
+            }
+            else
+            {
+                //Initialize renderer color, white?
+                SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+
+                //Initialize PNG loading
+                int imgFlags = IMG_INIT_PNG;
+                if( !( IMG_Init( imgFlags ) & imgFlags ) )
+                {
+                    printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
+                    success = false;
+                }
+
+                //  //Initialize SDL_ttf
+                // if( TTF_Init() == -1 )
+                // {
+                //     printf( "SDL_ttf could not initialize! SDL_ttf Error: %s\n", TTF_GetError() );
+                //     success = false;
+                // }
+            }
+        }
+    }
+
+    return success;
+    //Filled gWindow, gRenderer
+}
+
+bool loadMedia()
+{
+    //Loading success flag
+    bool success = true;
+
+    //Loading from resource
+    if( !gUpTexture.loadFromFile( "18_key_states/up.png" ) )
+    {
+        printf( "Failed to load up sprite sheet texture!\n" );
+        success = false;
+    }
+    else if ( !gDownTexture.loadFromFile( "18_key_states/down.png" ) )
+    {
+        printf( "Failed to load down sprite sheet texture!\n" );
+        success = false;
+    }
+    else if ( !gLeftTexture.loadFromFile( "18_key_states/left.png" ) )
+    {
+        printf( "Failed to load left sprite sheet texture!\n" );
+        success = false;
+    }
+    else if ( !gRightTexture.loadFromFile( "18_key_states/right.png" ) )
+    {
+        printf( "Failed to load right sprite sheet texture!\n" );
+        success = false;
+    }
+    else if ( !gPressTexture.loadFromFile( "18_key_states/press.png" ) )
+    {
+        printf( "Failed to load press sprite sheet texture!\n" );
+        success = false;
+    }
+
+    return success;
+}
+
+void close()
+{
+    //Free loaded image
+    gUpTexture.free();
+    gDownTexture.free();
+    gLeftTexture.free();
+    gRightTexture.free();
+    gPressTexture.free();
+    
+    //Destroy window
+    SDL_DestroyRenderer( gRenderer );
+    SDL_DestroyWindow( gWindow );
+    gRenderer = NULL;
+    gWindow = NULL;
+
+    //Quit SDL subsystems
+    //Clean up all dynamically loaded library handles, freeing memory
+    IMG_Quit();
+    SDL_Quit();
+}
+
+
+int main( int argc, char* args[] )
+{
+	//Start up SDL and create window
+	if( !init() )
+	{
+		printf( "Failed to initialize!\n" );
+	}
+	else
+	{
+		//Load media
+		if( !loadMedia() )
+		{
+			printf( "Failed to load media!\n" );
+		}
+		else
+		{	
+			//Main loop flag
+            bool quit = false;
+
+            //Event handler
+            SDL_Event e;
+
+            //Current rendered texture
+            LTexture* currentTexture = NULL;
+
+			//While application is running
+			while( !quit )
+            {
+                //Handle events on queue
+                while( SDL_PollEvent( &e ) != 0 )
+                {
+                    //User requests quit
+                    if( e.type == SDL_QUIT )
+                    {
+                        quit = true;
+                    }
+                }
+
+                //Set texture based on current keystate
+                const Uint8* currentKeyStates = SDL_GetKeyboardState( NULL );
+                if( currentKeyStates[ SDL_SCANCODE_UP ] )
+                {
+                    currentTexture = &gUpTexture;
+                }
+                else if( currentKeyStates[ SDL_SCANCODE_DOWN ] )
+                {
+                    currentTexture = &gDownTexture;
+                }
+                else if( currentKeyStates[ SDL_SCANCODE_LEFT ] )
+                {
+                    currentTexture = &gLeftTexture;
+                }
+                else if( currentKeyStates[ SDL_SCANCODE_RIGHT ] )
+                {
+                    currentTexture = &gRightTexture;
+                }
+                else
+                {
+                    currentTexture = &gPressTexture;
+                }
+
+                //Clear screen
+                SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+                SDL_RenderClear( gRenderer );
+
+                //Render current texture
+                currentTexture->render( 0, 0 );
+
+                //Update screen
+                SDL_RenderPresent( gRenderer );
+            }
+		}
+	}
+
+	//Free resources and close SDL
+	close();
+
+	return 0;
+}
